ad42af15cd0090269e19c03f3c28236222dfc461
HigasaOR <lee1003094395@gmail.com>
2024-04-09 08:36:10 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chien Lee on 2024/4/9.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
