d793a6f7e47bcd05792c548ec34d707d4db3b8f8
HigasaOR <lee1003094395@gmail.com>
2024-04-14 22:14:23 +0800

    finished lecture 4 progress

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -8,14 +8,27 @@
 import Foundation
 
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set) var cards: Array<Card>
     
-    func choose(card: Card){
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        for pairIndex in 0..<max(2, numberOfPairsOfCards) {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card) {
         
     } 
     
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
     struct Card {
-        var isFaceUp = false
+        var isFacedUp = true
         var isMatched = false
         var content: CardContent
     }
